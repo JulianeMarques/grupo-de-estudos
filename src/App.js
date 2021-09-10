81591fdee035e722e503c7f40441528f8393a439
Juliane Marques <mqs.juliane@gmail.com>
2021-09-09 21:21:44 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import TodoList from './pages/TodoList';
+import Login from './pages/Login'; 
+
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/TodoList" component={ TodoList } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/TodoList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo List</h1>
+            </div>
+        )
+    }
+}
+
+
+export default TodoList;
